ba909aa025020768ff2e01d5f3f8bac20b8230d6
Artem Rakov <scoff1k@gmail.com>
2017-12-15 04:03:38 +0000

    send response to db to save the card

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -22,12 +22,27 @@ export default {
   props: ['original_lists'],
   data() {
     return {
-      messages: {}
+      messages: {},
+      lists: this.original_lists
     }
   },
   methods: {
     submitMessages(list_id) {
-      console.log(this.messages[list_id])
+      var data = new FormData
+      data.append('card[list_id]', list_id)
+      data.append('card[name]', this.messages[list_id])
+
+      Rails.ajax({
+        url: '/cards',
+        type: 'POST',
+        data: data,
+        dataType: 'json',
+        success: (data) => {
+          const index = this.lists.findIndex(item => item.id === list_id)
+          this.lists[index].cards.push(data)
+          this.messages[list_id] = undefined
+        }
+      })
     }
   }
 
@@ -44,6 +59,7 @@ p {
   border: 1px solid black;
   padding: 10px 3px;
   text-align: center;
+  margin: 10px 0;
 }
 
 .card-form {
